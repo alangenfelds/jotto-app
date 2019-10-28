fc847def77d7e8be3f05710e406fa0f13d493945
Artūrs Langenfelds <arturs.langenfelds@abc>
2019-10-28 14:29:20 +0200

    wip

--- my-jotto-redux/src/components/GuessedWords.jsx
+++ my-jotto-redux/src/components/GuessedWords.jsx
@@ -2,18 +2,34 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const GuessedWords = props => {
-
-    const { guessedWords } = props;
+  const { guessedWords } = props;
 
   return (
     <div className="" data-test="component-guessed-words">
-     { guessedWords.length === 0 ?
-      <span data-test="component-guessed-instructions">Try to guess the secret word</span>
-      :
-      <div className="component-guessed-table">
-          <table></table>
-      </div>
-    }
+      {guessedWords.length === 0 ? (
+        <span data-test="component-guessed-instructions">
+          Try to guess the secret word
+        </span>
+      ) : (
+        <div data-test="guessed-words-section">
+          <table>
+            <thead>
+              <tr>
+                <th>Guess</th>
+                <th>Matching Letters</th>
+              </tr>
+            </thead>
+            <tbody>
+              {guessedWords.map((word, idx) => (
+                <tr key={idx} data-test="guessed-word">
+                  <td>{word.guessedWord}</td>
+                  <td>{word.letterMatchCount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
--- my-jotto-redux/src/components/GuessedWords.test.js
+++ my-jotto-redux/src/components/GuessedWords.test.js
@@ -35,11 +35,40 @@ describe("if the are no words guessed", () => {
   });
 
   test("renders instructions to guess the word", () => {
-    const instructions = findByTestAttr(wrapper, "component-guessed-instructions");
+    const instructions = findByTestAttr(
+      wrapper,
+      "component-guessed-instructions"
+    );
     expect(instructions.text().length).not.toBe(0);
   });
 });
 
-describe("if the are some words guessed", () => {});
+describe("if the are some words guessed", () => {
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: "train", letterMatchCount: 3 },
+    { guessedWord: "party", letterMatchCount: 5 },
+    { guessedWord: "drink", letterMatchCount: 1 }
+  ];
+
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+
+  test("renders without error", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+
+  test("renders guest words section", () => {
+    const guessedWordsDiv = findByTestAttr(wrapper, "guessed-words-section");
+    expect(guessedWordsDiv.length).toBe(1);
+  });
+
+  test("correct number of guessed words", () => {
+    const guessedWordsRows = findByTestAttr(wrapper, "guessed-word");
+    expect(guessedWordsRows.length).toBe(guessedWords.length);
+  });
+});
 
 describe("if word is guessed", () => {});
