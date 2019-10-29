26142f970d2ec26843975f7726bb0c810b4267c9
Artūrs Langenfelds <arturs.langenfelds@abc>
2019-10-29 10:02:39 +0200

    added some input tests

--- my-jotto-redux/src/components/Input.jsx
+++ my-jotto-redux/src/components/Input.jsx
@@ -3,14 +3,33 @@ import { connect } from "react-redux";
 
 class Input extends Component {
   render() {
-    return <div>
-        <button>Submit</button>
-    </div>;
+    const { success } = this.props;
+    return (
+      <div data-test="component-input">
+        {success ? null : (
+          <form className="form-inline">
+            <input
+              data-test="input-box"
+              type="text"
+              className="mb-2 ms-sm-3"
+              placeholder="enter guess"
+            />
+            <button
+              data-test="submit-button"
+              type="submit"
+              className="btn btn-primary mb-2"
+            >
+              Submit
+            </button>
+          </form>
+        )}
+      </div>
+    );
   }
 }
 
 const mapStateToProps = state => {
-  return {};
+  return { success: state.success };
 };
 
 export default connect(mapStateToProps)(Input);
